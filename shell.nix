59db452a504bffe975142791343ad723e40a8d38
Geoffrey Huntley <ghuntley@ghuntley.com>
2021-10-27 11:28:14 +1000

    Create shell.nix

---
+++ shell.nix
@@ -0,0 +1,15 @@
+# https://nix.dev/tutorials/declarative-and-reproducible-developer-environments
+with import <nixpkgs> { };
+
+mkShell {
+
+  # Package names can be found via https://search.nixos.org/packages
+  nativeBuildInputs = [
+    direnv
+  ];
+
+  NIX_ENFORCE_PURITY = true;
+
+  shellHook = ''
+  '';
+}
